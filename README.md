37d5b21031cb8d926957f97cf2c6a2f424b2b3c8
siraj chaudhary <mr.sirajchaudhary@yahoo.com>
2024-09-20 19:16:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"## Its a simple springboot microservice for testing purpose from other projects (e.g. Jenkins Job). It has a simple /hello RESTful API" 
---
+++ src/main/java/com/sirajchaudhary/helloworld/SpringbootHelloworldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sirajchaudhary.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldServiceApplication.class, args);
+	}
+
+}
