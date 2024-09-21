4c55783ddbb34829e582d25fd4ab0c6cab67d021
SirajChaudhary <76120970+SirajChaudhary@users.noreply.github.com>
2024-09-21 12:40:12 +0530

    Update HelloWorldController.java

--- src/main/java/com/sirajchaudhary/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/sirajchaudhary/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello World from T Raj";
+		return "Hello World from Khadse";
 	}
 }
